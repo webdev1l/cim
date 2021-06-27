ce7a982ef75bef942f2aa6c841906f08f897519f
webdev1l <samofalov.r.a@gmail.com>
2021-06-27 22:53:36 +0300

    commit

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,34 +1,48 @@
-const directions = document.querySelector('.directions');
+// const directions = document.querySelector('.directions');
+//
+// let prevItemId = null;
+//
+// if (directions) {
+//   directions.addEventListener('click', (event) => {
+//     const item = event.target.closest('.directions__item');
+//     if (item) {
+//       const itemId = item.getAttribute('data-id');
+//       if ((prevItemId !== itemId) && (prevItemId !== null)) {
+//         directions.querySelector(`[data-hidden-id="${prevItemId}"]`)
+//           .classList.remove('active');
+//         directions.querySelector(`[data-id="${prevItemId}"]`)
+//           .classList.remove('active');
+//       }
+//       prevItemId = itemId;
+//       const hiddenText = directions.querySelector(`[data-hidden-id="${itemId}"]`);
+//       item.classList.toggle('active');
+//       hiddenText.classList.toggle('active');
+//     }
+//   });
+// }
+//
+// const offside = offside('#my-menu', {
+//   slidingSide: 'right',
+//   buttonsSelector: '.toggle-button, .offside__close-btn',
+// });
+//
+// document.body.addEventListener('click', (event) => {
+//     if(!event.target.closest('.offside')
+//       && !event.target.closest('.toggle-button')) {
+//       offside.close()
+//     }
+// });
 
-let prevItemId = null;
+const fileInput = document.getElementById('file-upload');
 
-if (directions) {
-  directions.addEventListener('click', (event) => {
-    const item = event.target.closest('.directions__item');
-    if (item) {
-      const itemId = item.getAttribute('data-id');
-      if ((prevItemId !== itemId) && (prevItemId !== null)) {
-        directions.querySelector(`[data-hidden-id="${prevItemId}"]`)
-          .classList.remove('active');
-        directions.querySelector(`[data-id="${prevItemId}"]`)
-          .classList.remove('active');
-      }
-      prevItemId = itemId;
-      const hiddenText = directions.querySelector(`[data-hidden-id="${itemId}"]`);
-      item.classList.toggle('active');
-      hiddenText.classList.toggle('active');
+if (fileInput) {
+  fileInput.addEventListener('change', () => {
+    if (fileInput.files.length !== 0) {
+      const fileName = fileInput.files[0]['name'];
+      const fileData = document.querySelector('.form-section__file-data');
+      const fileLabel = document.querySelector('.form-section__file-upload');
+      fileLabel.innerHTML = 'Выбрать другой файл';
+      fileData.innerHTML = `Выбран файл ${fileName}`;
     }
-  });
+  })
 }
-
-const offside = offside('#my-menu', {
-  slidingSide: 'right',
-  buttonsSelector: '.toggle-button, .offside__close-btn',
-});
-
-document.body.addEventListener('click', (event) => {
-    if(!event.target.closest('.offside')
-      && !event.target.closest('.toggle-button')) {
-      offside.close()
-    }
-  });
